60929823b928e4cd20506255e93dacf55e35abe4
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2023-05-03 07:52:52 -0500

    building out orm models

---
+++ src/app/sql_app/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timezone
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, UUID
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+
+from .database import Base
+
+
+class Lift(Base):
+    __tablename__ = "lifts"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    lift_key: Mapped[UUID] = mapped_column(unique=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
+
+    # id = Column(Integer, primary_key=True, index=True)
+    # lift_key = Column(UUID, unique=True, index=True)
+    # user_id = Column(Integer, foreign_key index=True)
+    # name = Column(String, unique=True, index=True)
+    # training_max = Column(Integer)
+    # is_active = Column(Boolean, default=True)
+    # is_deleted = Column(Boolean, default=False)
+    # created_date = Column(DateTime, default=datetime.now(timezone.utc))
+    # modified_date = Column(DateTime, default=datetime.now(timezone.utc))
+    # created_by_user = Column(UUID)
