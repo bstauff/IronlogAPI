550585b0134ef0768a0baf065d4933c4f9d5ad35
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2023-05-03 07:02:34 -0500

    database class

---
+++ src/app/sql_app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:p@ssw0rd@localhost:5432/ironlog"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
