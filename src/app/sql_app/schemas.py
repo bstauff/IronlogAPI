4a5d82f3ea387e8ef37b23b4bcb758a57fc6501f
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2023-05-05 07:54:27 -0500

    pydantic schemas

---
+++ src/app/sql_app/schemas.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+import uuid
+from pydantic import BaseModel
+
+
+class LiftBase(BaseModel):
+    name: str
+    training_max: int
+
+
+class Lift(LiftBase):
+    id: int
+    user_id: int
+    lift_key: uuid.UUID
+    is_active: bool
+    is_deleted: bool
+    created_date: datetime
+    modified_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class LiftCreate(BaseModel):
+    user_id: int
+
+
+class UserBase(BaseModel):
+    pass
+
+
+class User(UserBase):
+    id: int
+    user_key: uuid.UUID
+    is_active: bool
+    is_deleted: bool
+    created_date: datetime
+    modified_date: datetime
+    lifts: list[Lift] = []
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    pass
