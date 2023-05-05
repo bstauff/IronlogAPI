3ad6b91d7de953d7faf60dc5d89391ba418f3ee3
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2023-05-05 07:27:49 -0500

    completing sqlalch models

--- src/app/sql_app/models.py
+++ src/app/sql_app/models.py
@@ -1,9 +1,25 @@
 from datetime import datetime, timezone
-from sqlalchemy import Boolean, Column, DateTime, Integer, String, UUID
+from sqlalchemy import Boolean, DateTime, Integer, String, UUID
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship, mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_key: Mapped[UUID] = mapped_column(unique=True, index=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_date: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now(timezone.utc)
+    )
+    modified_date: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now(timezone.utc)
+    )
+    lifts: Mapped[list["Lift"]] = relationship()
 
 
 class Lift(Base):
@@ -11,7 +27,6 @@ class Lift(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     lift_key: Mapped[UUID] = mapped_column(unique=True, index=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
     name: Mapped[str] = mapped_column(String, index=True)
     training_max: Mapped[int] = mapped_column(Integer)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
@@ -22,14 +37,4 @@ class Lift(Base):
     modified_date: Mapped[datetime] = mapped_column(
         DateTime, default=datetime.now(timezone.utc)
     )
-
-    # id = Column(Integer, primary_key=True, index=True)
-    # lift_key = Column(UUID, unique=True, index=True)
-    # user_id = Column(Integer, foreign_key index=True)
-    # name = Column(String, unique=True, index=True)
-    # training_max = Column(Integer)
-    # is_active = Column(Boolean, default=True)
-    # is_deleted = Column(Boolean, default=False)
-    # created_date = Column(DateTime, default=datetime.now(timezone.utc))
-    # modified_date = Column(DateTime, default=datetime.now(timezone.utc))
-    # created_by_user = Column(UUID)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
