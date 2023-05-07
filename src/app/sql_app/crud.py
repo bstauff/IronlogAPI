f4be3b43ef630adec40fcb74d33b32b2482bcd45
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2023-05-07 16:31:23 -0500

    building out crud class

---
+++ src/app/sql_app/crud.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+from uuid import uuid
+
+
+def get_user(db: Session, user_id: int) -> models.User:
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate) -> models.User:
+    db_user: models.User = models.User(user_key=uuid.uuid4())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
